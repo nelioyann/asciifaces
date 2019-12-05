f152683a9e18c2e584ee443c5f4b152770f293a4
nelioyann <nanayannick1998@gmail.com>
2019-12-05 04:57:27 +0100

    Updates

---
+++ precache-manifest.5282dcc99a28cbdcad33f58e20261f64.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8d8b8022efde179e2af699257509bc9",
+    "url": "/asciifaces/index.html"
+  },
+  {
+    "revision": "b89efc40faac051f6284",
+    "url": "/asciifaces/static/js/2.b163a724.chunk.js"
+  },
+  {
+    "revision": "1bffcf4f85584492c372",
+    "url": "/asciifaces/static/js/main.b26d6203.chunk.js"
+  },
+  {
+    "revision": "1133526c5924ae572de4",
+    "url": "/asciifaces/static/js/runtime~main.8c75b00d.js"
+  }
+]);
